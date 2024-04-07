b053cf79609984b4e16b0a050cb1f63d4d0c1050
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-07 15:23:18 -0500

    Add: routes admin

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,5 +1,9 @@
+import Heading from "@/components/ui/Heading";
+
 export default function OrdersPage() {
   return (
-    <div>page</div>
-  )
+    <>
+      <Heading>Administrar Ordenes</Heading>
+    </>
+  );
 }
--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -1,4 +1,5 @@
 import Logo from "../ui/Logo";
+import AdminRoute from "./AdminRoute";
 
 const adminNavigation = [
   { url: "/admin/orders", text: "Ordenes", blank: false },
@@ -14,7 +15,16 @@ export default function AdminSidebar() {
         <p className="mt-10 uppercase font-bold text-sm text-gray-600 text-center">
           Navegación
         </p>
-        <nav className="flex flex-col"></nav>
+        <nav className="flex flex-col">
+          {
+            adminNavigation.map(link => (
+              <AdminRoute 
+                key={link.url}
+                link={link}
+              />
+            ))
+          }
+        </nav>
       </div>
     </>
   );
