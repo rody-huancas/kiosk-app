0aebada387721c86695365f62dc56a17f510a5d9
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-07 15:38:57 -0500

    Add: list order pending

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,9 +1,42 @@
+import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function OrdersPage() {
+async function getPendingOrder () {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+  return orders;
+}
+
+export default async function OrdersPage() {
+  const orders = await getPendingOrder();
+
   return (
     <>
       <Heading>Administrar Ordenes</Heading>
+      {
+        orders.length ? (
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+            {
+              orders.map((order) => (
+                <OrderCard key={order.id} order={order} />
+              ))
+            }
+          </div>
+        ) : (
+          <p className="text-center">No hay ordenes pendiente</p>
+        )
+      }
     </>
   );
 }
