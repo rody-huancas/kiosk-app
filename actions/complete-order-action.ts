46e587e77bc7dd5956f880c091dc03215886384f
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-08 19:04:08 -0500

    Add: refetch of data orders

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -2,24 +2,26 @@
 
 import { prisma } from "@/src/lib/prisma";
 import { OrderIdSchema } from "@/src/schema";
+import { revalidatePath } from "next/cache";
 
 export async function completeOrder(formData: FormData) {
   const data = {
-    orderId: formData.get('order_id')!
+    orderId: formData.get("order_id")!,
   };
 
   const result = OrderIdSchema.safeParse(data);
-  if(result.success) {
+  if (result.success) {
     try {
       await prisma.order.update({
-          where: {
-            id: result.data.orderId
-          },
-          data: {
-            status: true,
-            orderReadyAt: new Date(Date.now())
-          },
-        });
+        where: {
+          id: result.data.orderId,
+        },
+        data: {
+          status: true,
+          orderReadyAt: new Date(Date.now()),
+        },
+      });
+      revalidatePath('/admin/orders')
     } catch (error) {
       console.log(error);
     }
