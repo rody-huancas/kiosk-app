b20668db2d0105b5c8c731551cfbb0fe3e44f51a
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-10 19:37:56 -0500

    Fix: validate total pages

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,6 +2,7 @@ import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { redirect } from "next/navigation";
 
 async function productCount() {
   return await prisma.product.count();
@@ -26,11 +27,15 @@ export default async function ProductsPage({searchParams}: {searchParams: {page:
   const page = +searchParams.page || 1;
   const pageSize = 10;
   
+  if(page < 0) redirect('/admin/products');
+
   const productsData = getProducts(page, pageSize);
   const totalProductsData = productCount();
   const [products, totalProducts] = await Promise.all([productsData, totalProductsData]);
   const totalPages = Math.ceil(totalProducts / pageSize);
 
+  if(page > totalPages) redirect('/admin/products');
+  
   return (
     <>
       <Heading>Administrar Productos</Heading>
