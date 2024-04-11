2c4f19e8888229381922b63c2c56bc99ba1daee7
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-10 19:34:25 -0500

    Add: pagination products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,17 @@
+import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts() {
+async function productCount() {
+  return await prisma.product.count();
+}
+
+async function getProducts(page: number, pageSize: number) {
+  const skip = (page - 1) * pageSize;
   const products = await prisma.product.findMany({
+    take: pageSize,
+    skip,
     include: {
       category: true,
     }
@@ -14,8 +22,14 @@ async function getProducts() {
 
 export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
 
-export default async function ProductsPage() {
-  const products = await getProducts();
+export default async function ProductsPage({searchParams}: {searchParams: {page: string}}) {
+  const page = +searchParams.page || 1;
+  const pageSize = 10;
+  
+  const productsData = getProducts(page, pageSize);
+  const totalProductsData = productCount();
+  const [products, totalProducts] = await Promise.all([productsData, totalProductsData]);
+  const totalPages = Math.ceil(totalProducts / pageSize);
 
   return (
     <>
@@ -24,6 +38,8 @@ export default async function ProductsPage() {
       <ProductTable
         products={products}
       />
+
+      <ProductsPagination page={page} totalPages={totalPages} />
     </>
   );
 }
