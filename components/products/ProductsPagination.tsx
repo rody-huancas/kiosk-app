aedf1a9e8a70554d6cbf13bc0672908a40013423
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-12 18:55:36 -0500

    Add: search with name

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -22,6 +22,7 @@ export default function ProductsPagination({ page, totalPages }: ProductsPaginat
       {
         pages.map(currentPage => (
           <Link
+            key={currentPage}
             href={`/admin/products?page=${currentPage}`}
             className={`${page === currentPage && 'font-black'} bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}
           >
