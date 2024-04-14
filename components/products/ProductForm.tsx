1a4e2a208d24e33f785e3a02b30c4cba33bf4d6b
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-14 16:09:50 -0500

    Add: configuration cloudinary

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/src/lib/prisma";
+import ImageUpload from "./ImageUpload";
 
 async function getCategories() {
   return await prisma.category.findMany();
@@ -53,6 +54,8 @@ export default async function ProductForm() {
           }
         </select>
       </div>
+
+      <ImageUpload />
     </>
   );
 }
