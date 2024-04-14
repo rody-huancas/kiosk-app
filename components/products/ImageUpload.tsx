3adb96d8315707cdb370028b6de054a486eea7a9
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-14 16:28:51 -0500

    Add: upload image in cloudinary

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,5 +1,53 @@
+"use client";
+
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useState } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
 export default function ImageUpload() {
+  const [imageUrl, setImageUrl] = useState("");
+
   return (
-    <div>ImageUpload</div>
-  )
+    <CldUploadWidget
+      onSuccess={(result, { widget }) => {
+        if (result.event === "success") {
+          widget.close();
+          // @ts-ignore
+          setImageUrl(result.info?.secure_url);
+        }
+      }}
+      uploadPreset="n2qqpclj"
+      options={{
+        maxFiles: 1,
+      }}
+    >
+      {({ open }) => (
+        <>
+          <div className="space-y-2">
+            <label className="text-slate-800">Imagen Producto</label>
+            <div
+              onClick={() => open()}
+              className="relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100"
+            >
+              <TbPhotoPlus size={50} />
+              <p className="text-lg font-semibold">Agregar Imagen</p>
+              {imageUrl && (
+                <div className="absolute inset-0 w-full h-full">
+                  <Image
+                    fill
+                    style={{ objectFit: "contain" }}
+                    src={imageUrl}
+                    alt="Imagen de producto"
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <input type="hidden" name="image" value={imageUrl} />
+        </>
+      )}
+    </CldUploadWidget>
+  );
 }
