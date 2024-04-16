155b7ab735bbc08c65e317bdd4c76a8b88443ca8
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-15 20:07:48 -0500

    Add: create new product

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,14 +1,18 @@
 "use client"
 
+import { createProduct } from "@/actions/create-product-action";
 import { ProductSchema } from "@/src/schema";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
 export default async function AddProductForm({children}: {children: React.ReactNode}) {
+  const router = useRouter();
   const handleSubmit = async (formData: FormData) => {
     const data = {
       name: formData.get("name"),
       price: formData.get("price"),
       categoryId: formData.get("categoryId"),
+      image: formData.get("image"),
     }
     const result = ProductSchema.safeParse(data);
     
@@ -19,6 +23,18 @@ export default async function AddProductForm({children}: {children: React.ReactN
 
       return;
     }
+
+    const response = await createProduct(result.data);
+
+    if(response?.errors) {
+      response.errors.forEach(issue => {
+        toast.error(issue.message);
+      });
+      return;
+    }
+
+    toast.success("Producto creado correctamente!")
+    router.push("/admin/products")
   }
 
   return (
