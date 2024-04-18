bdec158d9cd6a9da21853ef1f97cecbdbb2b2bbe
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-17 19:58:24 -0500

    Add: view image uploaded in product edit

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import { getImagePath } from "@/src/utils";
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useState } from "react";
 import { TbPhotoPlus } from "react-icons/tb";
 
-export default function ImageUpload() {
+export default function ImageUpload({image}: {image: string | undefined}) {
   const [imageUrl, setImageUrl] = useState("");
 
   return (
@@ -45,7 +46,22 @@ export default function ImageUpload() {
             </div>
           </div>
 
-          <input type="hidden" name="image" value={imageUrl} />
+          {
+            image && !imageUrl && (
+              <div className="space-y-2">
+                <label>Imagen Actual</label>
+                <div className="relative w-64 h-64">
+                  <Image 
+                    fill
+                    src={getImagePath(image)}
+                    alt="Imagen producto"
+                  />
+                </div>
+              </div>
+            )
+          }
+
+          <input type="hidden" name="image" defaultValue={imageUrl} />
         </>
       )}
     </CldUploadWidget>
