ac1f4f613258f46d76af457efeda60bbaa19d25d
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-17 19:52:28 -0500

    Add: button in edit for go back

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,7 +1,9 @@
 import EditProductForm from "@/components/products/EditProductForm";
 import ProductForm from "@/components/products/ProductForm";
+import GoBackButton from "@/components/ui/GoBackButton";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 async function getProductById(id: number) {
@@ -21,6 +23,8 @@ export default async function EditProductsPage({ params }: {  params: { id: stri
   return (
     <>
       <Heading>Editar Producto: {product.name}</Heading>
+
+      <GoBackButton />
 
       <EditProductForm>
         <ProductForm product={product} />
