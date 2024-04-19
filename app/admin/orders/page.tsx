64b0019cc935f6af977f4d2cbada9526b733b71b
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-18 20:35:43 -0500

    Add: manual revalidation in orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,7 @@
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 async function getPendingOrder () {
   const orders = await prisma.order.findMany({
@@ -21,9 +22,25 @@ async function getPendingOrder () {
 export default async function OrdersPage() {
   const orders = await getPendingOrder();
 
+  const refreshOrders = async () => {
+    "use server"
+    revalidatePath("/admin/orders")
+  }
+
   return (
     <>
       <Heading>Administrar Ordenes</Heading>
+
+      <form
+        action={refreshOrders}
+      >
+        <input 
+          type="submit" 
+          value="Actualizar Ordenes"
+          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+        />
+      </form>
+
       {
         orders.length ? (
           <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
