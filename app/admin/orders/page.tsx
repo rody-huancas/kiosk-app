d1f118abf5fddce461ab64c4b8ceb67a9f296857
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-19 19:25:59 -0500

    Add: SWR for orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,59 +1,29 @@
+"use client";
+
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
-import { revalidatePath } from "next/cache";
+import { OrderWithProducts } from "@/src/types";
+import useSWR from "swr";
 
-async function getPendingOrder () {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      },
-    },
-  });
-  return orders;
-}
+export default function OrdersPage() {
+  const url = "/admin/orders/api";
+  const fetcher = () => fetch(url).then((res) => res.json()).then((data) => data);
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>( url, fetcher, {} );
 
-export default async function OrdersPage() {
-  const orders = await getPendingOrder();
+  if (data)
+    return (
+      <>
+        <Heading>Administrar Ordenes</Heading>
 
-  const refreshOrders = async () => {
-    "use server"
-    revalidatePath("/admin/orders")
-  }
-
-  return (
-    <>
-      <Heading>Administrar Ordenes</Heading>
-
-      <form
-        action={refreshOrders}
-      >
-        <input 
-          type="submit" 
-          value="Actualizar Ordenes"
-          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
-        />
-      </form>
-
-      {
-        orders.length ? (
+        {data.length ? (
           <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-            {
-              orders.map((order) => (
-                <OrderCard key={order.id} order={order} />
-              ))
-            }
+            {data.map((order) => (
+              <OrderCard key={order.id} order={order} />
+            ))}
           </div>
         ) : (
           <p className="text-center">No hay ordenes pendiente</p>
-        )
-      }
-    </>
-  );
+        )}
+      </>
+    );
 }
