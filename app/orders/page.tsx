6d50b8eb6fb998d8fe274e1a591dc582d8e94543
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-20 19:14:53 -0500

    Add: endopoint for show orders

---
+++ app/orders/page.tsx
@@ -0,0 +1,10 @@
+import Logo from "@/components/ui/Logo";
+
+export default function OrdersPage() {
+  return (
+    <>
+      <h1 className="text-center mt-20 text-6xl font-black text-gray-900/90">Ordenes Listas</h1>
+      <Logo />
+    </>
+  );
+}
