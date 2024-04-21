c541dcaefbb7d8550b8d8fdcb8001f5c345ffcd3
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-20 19:33:16 -0500

    Add: list of ready orders

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,10 +1,40 @@
+"use client";
+
+import LatesOrderItem from "@/components/order/LatesOrderItem";
 import Logo from "@/components/ui/Logo";
+import { OrderWithProducts } from "@/src/types";
+import useSWR from "swr";
 
 export default function OrdersPage() {
-  return (
-    <>
-      <h1 className="text-center mt-20 text-6xl font-black text-gray-900/90">Ordenes Listas</h1>
-      <Logo />
-    </>
-  );
+  const url = "/orders/api";
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 60000,
+    revalidateOnFocus: false,
+  });
+
+  if (isLoading) return <p>Cargando...</p>;
+
+  if (data)
+    return (
+      <>
+        <h1 className="text-center mt-20 text-6xl font-black text-gray-900/90">
+          Ordenes Listas
+        </h1>
+        <Logo />
+
+        {data.length ? (
+          <div className="grid grid-cols-2 gap-5 max-w-5xl mx-auto mt-10">
+            {data.map((order) => (
+              <LatesOrderItem key={order.id} order={order} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center my-10">No hay ordenes listas</p>
+        )}
+      </>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <h1>Hola mundo</h1>
-  );
+  redirect("/order/cafe")
 }
